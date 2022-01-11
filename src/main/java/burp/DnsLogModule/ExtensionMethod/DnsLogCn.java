ac2f6a61b82bb6408e9d0e03df4d418ac408ccb6
pmiaowu <3303003493@qq.com>
2022-01-11 09:55:48 +0800

    代码优化

--- src/main/java/burp/DnsLogModule/ExtensionMethod/DnsLogCn.java
+++ src/main/java/burp/DnsLogModule/ExtensionMethod/DnsLogCn.java
@@ -2,6 +2,7 @@ package burp.DnsLogModule.ExtensionMethod;
 
 import java.io.PrintWriter;
 
+import burp.Bootstrap.CustomHelpers;
 import com.github.kevinsawicki.http.HttpRequest;
 
 import burp.IBurpExtenderCallbacks;
@@ -61,7 +62,7 @@ public class DnsLogCn extends DnsLogAbstract {
 
         String cookie = request.header("Set-Cookie");
         String sessidKey = "PHPSESSID";
-        String sessidValue = getParam(cookie, sessidKey);
+        String sessidValue = CustomHelpers.getParam(cookie, sessidKey);
         if (sessidValue.length() == 0) {
             throw new IllegalArgumentException(
                     String.format(
@@ -137,52 +138,5 @@ public class DnsLogCn extends DnsLogAbstract {
         stdout.println(String.format("dnsLog临时域名: %s", this.getTemporaryDomainName()));
         stdout.println("===================================");
         stdout.println("");
-    }
-
-    /**
-     * 获取参数数据
-     * 例如:
-     * getParam("token=xx;Identifier=xxx;", "token"); 返回: xx
-     *
-     * @param d         被查找的数据
-     * @param paramName 要查找的字段
-     * @return
-     */
-    private static String getParam(final String d, final String paramName) {
-        if (d == null || d.length() == 0)
-            return null;
-
-        String value = "test=test;" + d;
-
-        final int length = value.length();
-        int start = value.indexOf(';') + 1;
-        if (start == 0 || start == length)
-            return null;
-
-        int end = value.indexOf(';', start);
-        if (end == -1)
-            end = length;
-
-        while (start < end) {
-            int nameEnd = value.indexOf('=', start);
-            if (nameEnd != -1 && nameEnd < end
-                    && paramName.equals(value.substring(start, nameEnd).trim())) {
-                String paramValue = value.substring(nameEnd + 1, end).trim();
-                int valueLength = paramValue.length();
-                if (valueLength != 0)
-                    if (valueLength > 2 && '"' == paramValue.charAt(0)
-                            && '"' == paramValue.charAt(valueLength - 1))
-                        return paramValue.substring(1, valueLength - 1);
-                    else
-                        return paramValue;
-            }
-
-            start = end + 1;
-            end = value.indexOf(';', start);
-            if (end == -1)
-                end = length;
-        }
-
-        return null;
     }
 }
